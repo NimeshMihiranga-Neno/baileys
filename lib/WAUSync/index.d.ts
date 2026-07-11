b24243a54b836a6be2e05d18e9841f988973794b
Nimesh Mihiranga <nimeshmihiranga.doc@gmail.com>
2026-07-10 18:28:50 -0700

    Add files via upload

--- lib/WAUSync/index.d.ts
+++ lib/WAUSync/index.d.ts
@@ -1,4 +1,6 @@
-export * from "./Protocols/index.js";
-export * from "./USyncQuery.js";
-export * from "./USyncUser.js";
+export * from "./USyncDeviceProtocol.js";
+export * from "./USyncContactProtocol.js";
+export * from "./USyncStatusProtocol.js";
+export * from "./USyncDisappearingModeProtocol.js";
+export * from "./USyncUsernameProtocol.js";
 //# sourceMappingURL=index.d.ts.map
